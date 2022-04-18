c585c4b38caf19e567ad49a4e5d5817d2065bc25
Radu Moldovan <radu.ioan.moldovan94@gmail.com>
2022-04-18 14:09:15 +0300

    Next+Tailwind starter

---
+++ components/UI/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ThemeProvider } from "next-themes";
+
+export default function Layout({ children }: React.PropsWithChildren<{}>) {
+    return(
+        <React.Fragment>
+            <ThemeProvider attribute='class'>
+                <main>{children}</main>
+            </ThemeProvider>            
+        </React.Fragment>
+    );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+import { AppProps } from "next/app";
+import Layout from "../components/UI/Layout";
+import '../styles/globals.css';
+export default function MyApp({Component, pageProps}: AppProps) {
+    return(
+        <Layout>
+            <Component {...pageProps}/>
+        </Layout>
+    );
+};
